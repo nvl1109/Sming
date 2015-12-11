5701cf981ddb5b71b13c7e58234e9851a6be376f
Reinis Veips <reinis.veips@wot.lv>
2015-12-11 22:37:38 +0200

    Merges in HardwareSerial from esp8266/Arduino.

--- Sming/SmingCore/HardwareSerial.h
+++ Sming/SmingCore/HardwareSerial.h
@@ -1,138 +1,139 @@
-/****
- * Sming Framework Project - Open Source framework for high efficiency native ESP8266 development.
- * Created 2015 by Skurydin Alexey
- * http://github.com/anakod/Sming
- * All files of the Sming Core are provided under the LGPL v3 license.
- ****/
+/*
+ HardwareSerial.h - Hardware serial library for Wiring
+ Copyright (c) 2006 Nicholas Zambetti.  All right reserved.
 
-/**	@defgroup serial Hardware serial
- *	@brief	Access serial UART 0
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+ Modified 28 September 2010 by Mark Sproul
+ Modified 14 August 2012 by Alarus
+ Modified 3 December 2013 by Matthijs Kooijman
+ Modified 18 December 2014 by Ivan Grokhotkov (esp8266 platform support)
+ Modified 31 March 2015 by Markus Sattler (rewrite the code for UART0 + UART1 support in ESP8266)
+ Modified 25 April 2015 by Thomas Flayols (add configuration different from 8N1 in ESP8266)
  */
 
-#ifndef _HARDWARESERIAL_H_
-#define _HARDWARESERIAL_H_
+#ifndef HardwareSerial_h
+#define HardwareSerial_h
 
-#include "../Wiring/WiringFrameworkDependencies.h"
-#include "../Wiring/Stream.h"
-#include "../SmingCore/Delegate.h"
-#include "../Services/CommandProcessing/CommandProcessingIncludes.h"
+#include <inttypes.h>
+
+#include "Stream.h"
+
+#define SERIAL_TX_BUFFER_SIZE 256
+#define SERIAL_RX_BUFFER_SIZE 256
+
+// Define config for Serial.begin(baud, config);
+#define SERIAL_5N1 0x10
+#define SERIAL_6N1 0x14
+#define SERIAL_7N1 0x18
+#define SERIAL_8N1 0x1c
+#define SERIAL_5N2 0x30
+#define SERIAL_6N2 0x34
+#define SERIAL_7N2 0x38
+#define SERIAL_8N2 0x3c
+#define SERIAL_5E1 0x12
+#define SERIAL_6E1 0x16
+#define SERIAL_7E1 0x1a
+#define SERIAL_8E1 0x1e
+#define SERIAL_5E2 0x32
+#define SERIAL_6E2 0x36
+#define SERIAL_7E2 0x3a
+#define SERIAL_8E2 0x3e
+#define SERIAL_5O1 0x13
+#define SERIAL_6O1 0x17
+#define SERIAL_7O1 0x1b
+#define SERIAL_8O1 0x1f
+#define SERIAL_5O2 0x33
+#define SERIAL_6O2 0x37
+#define SERIAL_7O2 0x3b
+#define SERIAL_8O2 0x3f
+
+#define SERIAL_FULL     0
+#define SERIAL_RX_ONLY  1
+#define SERIAL_TX_ONLY  2
 
 #define UART_ID_0   0
 #define UART_ID_1   1
 
 #define NUMBER_UARTS 2
 
-#define SERIAL_SIGNAL_DELEGATE	0
-#define SERIAL_SIGNAL_COMMAND	1
-#define SERIAL_QUEUE_LEN		10
+class cbuf;
 
-// Delegate constructor usage: (&YourClass::method, this)
-typedef Delegate<void(Stream &source, char arrivedChar, uint16_t availableCharsCount)> StreamDataReceivedDelegate;
+struct uart_;
+typedef struct uart_ uart_t;
 
-class CommandExecutor;
+class HardwareSerial: public Stream {
+    public:
+        HardwareSerial(int uart_nr);
+
+        void begin(unsigned long baud) {
+            begin(baud, SERIAL_8N1, SERIAL_FULL);
+        }
+        void begin(unsigned long baud, uint8_t config) {
+            begin(baud, config, SERIAL_FULL);
+        }
+        void begin(unsigned long, uint8_t, uint8_t);
+        void end();
+        void swap();  //toggle between use of GPIO13/GPIO15 or GPIO3/GPIO1 as RX and TX
+        int available(void) override;
+        int peek(void) override;
+        int read(void) override;
+        int availableForWrite(void);
+        void flush(void) override;
+        size_t write(uint8_t) override;
+        inline size_t write(unsigned long n) {
+            return write((uint8_t) n);
+        }
+        inline size_t write(long n) {
+            return write((uint8_t) n);
+        }
+        inline size_t write(unsigned int n) {
+            return write((uint8_t) n);
+        }
+        inline size_t write(int n) {
+            return write((uint8_t) n);
+        }
+        using Print::write; // pull in write(str) and write(buf, size) from Print
+        operator bool() const;
+
+        void setDebugOutput(bool);
+        bool isTxEnabled(void);
+        bool isRxEnabled(void);
+
+        int readMemoryBlock(char* buf, int max_len);
+
+        void systemDebugOutput(bool enabled) {
+            setDebugOutput(enabled);
+        }
+
+        void commandProcessing(bool reqEnable);
+        //void setCallback(StreamDataReceivedDelegate reqCallback, bool useSerialRxBuffer = true);
+        void resetCallback();
 
 
-typedef struct
-{
-	StreamDataReceivedDelegate HWSDelegate;
-	bool useRxBuff;
-	CommandExecutor* commandExecutor = nullptr;
-} HWSerialMemberData;
+    protected:
+        friend void uart_interrupt_handler(uart_t* uart);
+        void _rx_complete_irq(char c);
+        void _tx_empty_irq(void);
 
-class HardwareSerial : public Stream
-{
-public:
-    /** @brief  Create instance of a hardware serial port object
-     *  @param  uartPort UART number [0 | 1]
-     *  @note   A global instance of UART 0 is already defined as Serial
-     *  @addtogroup serial
-     *  @{
-     */
-	HardwareSerial(const int uartPort);
-	~HardwareSerial() {}
-
-    /** @brief  Initialise the serial port
-     *  @param  baud BAUD rate of the serial port (Default: 9600)
-     */
-	void begin(const uint32_t baud = 9600);
-
-    /** @brief  Get quantity characters available from serial input
-     *  @retval int Quantity of characters in receive buffer
-     */
-	int available();
-
-    /** @brief  Read a character from serial port
-     *  @retval int Character read from serial port or -1 if buffer empty
-     *  @note   The character is removed from the serial port input buffer
-    */
-	int read();
-
-	/** @brief  Read a block of characters from serial port
-	 *  @param  buf Pointer to buffer to hold received data
-	 *  @param  max_len Maximum quantity of characters to read
-	 *  @retval int Quantity of characters read
-	 */
- 	int readMemoryBlock(char* buf, int max_len);
-
-    /** @brief  Read a character from serial port without removing from input buffer
-     *  @retval int Character read from serial port or -1 if buffer empty
-     *  @note   The character remains in serial port input buffer
-     */
-	int peek();
-
-	/** @brief  Clear the serial port receive buffer
- 	 *  @note   All received data is removed from the serial port buffer
- 	 *  @warning This function is not implemented!!!
- 	 *  @todo   Implement Serial::flush()
-	 */
-	void flush();
-
-	/** @brief  write a character to serial port
-	 *  @param  oneChar Character to write to the serial port
-	 *  @retval size_t Quantity of characters written (always 1)
-	 */
-	size_t write(uint8_t oneChar);
-
-	//void printf(const char *fmt, ...);
-	/** @brief  Configure serial port for system debug output
-	 *  @param  enabled True to enable this port for system debug output
-	 *  @note   If enabled, port will issue system debug messages
-	 */
-	void systemDebugOutput(bool enabled);
-
-	/** @brief  Configure serial port for command processing
-	 *  @param  reqEnable True to enable command processing
-	 *  @note   Command processing provides a CLI to the system
-	 *  @see    commandHandler
-	 */
-	void commandProcessing(bool reqEnable);
-
-	/** @brief  Set handler for received data
-	 *  @param  reqCallback Function to handle received data
-	 *  @param  useSerialRxBuffer True to use the built-in serial receive buffer
-	 */
-	void setCallback(StreamDataReceivedDelegate reqCallback, bool useSerialRxBuffer = true);
-
-	/** @brief  Remove handler for received data
-	 */
-	void resetCallback();
-
-    /** @brief  Interrupt handler for UART0 receive events
-     *  @todo   Should HardwareSerial::uart0_rx_intr_handler be private?
-     */
-	static void IRAM_ATTR uart0_rx_intr_handler(void *para);
-
-	/** @brief  Trigger task for input event
-	 *  @param  inputEvent The event to use for trigger
-	 *  @todo   Should HardwareSerial::delgateTask be private?
-	 */
-	static void delegateTask (os_event_t *inputEvent);
-
-private:
-	int uart;
-	static HWSerialMemberData memberData[NUMBER_UARTS];
-
-	os_event_t * serialQueue;
+    protected:
+        int _uart_nr;
+        uart_t* _uart;
+        cbuf* _tx_buffer;
+        cbuf* _rx_buffer;
+        bool _written;
 };
 
 /**	@brief	Global instance of serial port UART0
@@ -142,6 +143,6 @@ private:
 	@endcode
 */
 extern HardwareSerial Serial;
+extern HardwareSerial Serial1;
 
-/** @} */
-#endif /* _HARDWARESERIAL_H_ */
+#endif
